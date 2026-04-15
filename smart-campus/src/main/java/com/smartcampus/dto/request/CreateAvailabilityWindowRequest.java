09c533c0f6b4916d8e8f625d4320e886c996a292
mihiran13 <yosithamihiran@outlook.com>
2026-04-15 17:39:21 +0530

    Implement availability window management

---
+++ smart-campus/src/main/java/com/smartcampus/dto/request/CreateAvailabilityWindowRequest.java
@@ -0,0 +1,26 @@
+package com.smartcampus.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateAvailabilityWindowRequest {
+
+    @NotNull(message = "Day of week is required")
+    private DayOfWeek dayOfWeek;
+
+    @NotNull(message = "Start time is required")
+    private LocalTime startTime;
+
+    @NotNull(message = "End time is required")
+    private LocalTime endTime;
+}
